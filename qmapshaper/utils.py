87addbfc206ca9db2891b4e48754806dbbaf6d35
Jan Caha <jan.caha@outlook.com>
2022-01-29 17:46:48 +0100

    add functions

--- qmapshaper/utils.py
+++ qmapshaper/utils.py
@@ -1,14 +1,31 @@
-from typing import List
+import os
+from typing import List, Any
 from pathlib import Path
 import subprocess
+from pytest_qgis import QgsVectorLayer
 
-from qgis.core import (QgsProcessingFeedback)
+from qgis.core import (QgsProcessingFeedback, QgsMessageLog, Qgis)
 from processing.core.ProcessingConfig import ProcessingConfig
+
+from .text_constants import TextConstants
+
+LOG_DEV = False
+
+if os.environ.get("QMAPSHAPER_DEV"):
+    if os.environ.get("QMAPSHAPER_DEV").lower() == "true":
+        LOG_DEV = True
 
 
 class QMapshaperUtils:
 
     MAPSHAPER_FOLDER = "MAPSHAPER_FOLDER"
+
+    @staticmethod
+    def full_path_command(command: str) -> str:
+
+        path = Path(QMapshaperUtils.mapshaper_bin_folder()) / command
+
+        return path.absolute().as_posix()
 
     @staticmethod
     def mapshaper_bin_folder() -> str:
@@ -48,3 +65,30 @@ class QMapshaperUtils:
 
         feedback.pushInfo("Result {}.".format(res.stdout.readlines()))
         feedback.pushInfo("Command runned.")
+
+
+def get_icons_folder() -> Path:
+    return Path(__file__).parent / "icons"
+
+
+def get_icon_path(file_name: str) -> str:
+
+    file: Path = get_icons_folder() / file_name
+
+    return file.absolute().as_posix()
+
+
+def log(text: Any) -> None:
+    if LOG_DEV:
+        QgsMessageLog.logMessage(str(text), TextConstants.plugin_name, Qgis.Info)
+
+
+def features_count_with_non_empty_geoms(layer: QgsVectorLayer) -> int:
+
+    count = 0
+
+    for feature in layer.getFeatures():
+        if not feature.geometry().isEmpty():
+            count += 1
+
+    return count
