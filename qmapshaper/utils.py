5cc3b0899d050fd89c8896b4d0be38a5430b6311
Jan Caha <jan.caha@outlook.com>
2022-01-31 18:10:34 +0100

    leave only true util functions

--- qmapshaper/utils.py
+++ qmapshaper/utils.py
@@ -1,11 +1,10 @@
 import os
-from typing import List, Any
+from typing import Any
 from pathlib import Path
-import subprocess
+
 from pytest_qgis import QgsVectorLayer
 
 from qgis.core import (QgsProcessingFeedback, QgsMessageLog, Qgis)
-from processing.core.ProcessingConfig import ProcessingConfig
 
 from .text_constants import TextConstants
 
@@ -14,62 +13,6 @@ LOG_DEV = False
 if os.environ.get("QMAPSHAPER_DEV"):
     if os.environ.get("QMAPSHAPER_DEV").lower() == "true":
         LOG_DEV = True
-
-
-class QMapshaperCommandsUtils:
-
-    @staticmethod
-    def full_path_command(command: str) -> str:
-
-        path = Path(QMapshaperCommandsUtils.mapshaper_bin_folder()) / command
-
-        return path.absolute().as_posix()
-
-    @staticmethod
-    def mapshaper_bin_folder() -> str:
-
-        folder = Path(QMapshaperCommandsUtils.mapshaper_folder()) / "bin"
-
-        return folder.absolute().as_posix()
-
-    @staticmethod
-    def mapshaper_folder() -> str:
-
-        folder = ProcessingConfig.getSetting(TextConstants.MAPSHAPER_FOLDER)
-
-        if not folder:
-            folder = QMapshaperCommandsUtils.guess_mapshaper_folder()
-
-        return folder if folder else ''
-
-    @staticmethod
-    def guess_mapshaper_folder() -> str:
-
-        folder = Path.home() / "node_modules" / "mapshaper"
-
-        return folder.absolute().as_posix()
-
-    @staticmethod
-    def runMapshaper(commands: List[str], feedback: QgsProcessingFeedback = None):
-
-        if feedback:
-            feedback.pushInfo("Running command: {}".format(" ".join(commands)))
-
-        res = subprocess.Popen(commands,
-                               stdout=subprocess.PIPE,
-                               stdin=subprocess.DEVNULL,
-                               stderr=subprocess.STDOUT,
-                               universal_newlines=True)
-
-        if feedback:
-            feedback.pushInfo("Result: ")
-
-            lines = res.stdout.readlines()
-
-            for line in lines:
-                feedback.pushInfo("{}.".format(line))
-
-            feedback.pushInfo("Command runned.")
 
 
 def get_icons_folder() -> Path:
