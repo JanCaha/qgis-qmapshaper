e8daec5a87ae5acf1a7f02d0cc61cca01b7d9d6c
Jan Caha <jan.caha@outlook.com>
2022-02-02 13:31:57 +0100

    add test

--- tests/classes/test_qmapshaper_paths.py
+++ tests/classes/test_qmapshaper_paths.py
@@ -1,3 +1,6 @@
+from processing.core.ProcessingConfig import ProcessingConfig, Setting
+
+from qmapshaper.text_constants import TextConstants
 from qmapshaper.classes.class_qmapshaper_paths import QMapshaperPaths
 from qmapshaper.classes.class_qmapshaper_command_builder import QMapshaperCommandBuilder
 
@@ -27,3 +30,22 @@ def test_mapshaper_builder_command():
 
     assert QMapshaperCommandBuilder.mapshaper_command(
     ) == QMapshaperCommandBuilder.mapshaper_command_name()
+
+
+def test_with_folder_setting_set():
+
+    ProcessingConfig.addSetting(
+        Setting(TextConstants.plugin_name,
+                TextConstants.MAPSHAPER_FOLDER,
+                'Mapshaper folder',
+                "/usr/local",
+                valuetype=Setting.FOLDER))
+
+    ProcessingConfig.readSettings()
+
+    assert QMapshaperPaths.mapshaper_folder() == "/usr/local"
+    assert QMapshaperPaths.mapshaper_bin_folder() == "/usr/local/bin"
+
+    command = QMapshaperCommandBuilder.mapshaper_command_name()
+
+    assert QMapshaperPaths.full_path_command(command) == f"/usr/local/bin/{command}"
