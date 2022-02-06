f17d44d73090779ab3bba7882e3e04caebc27c1e
Jan Caha <jan.caha@outlook.com>
2022-02-06 11:42:08 +0000

    add tests for new tool

---
+++ tests/processing/test_tool_to_topojson.py
@@ -0,0 +1,106 @@
+from pathlib import Path
+
+from qgis.core import (QgsProcessingParameterVectorLayer, QgsProcessingParameterField,
+                       QgsProcessingParameterFileDestination, QgsVectorLayer,
+                       QgsProcessingFeedback, QgsProcessingContext, QgsProcessingOutputFile)
+
+from qmapshaper.processing.tool_to_topojson import ConvertToTopoJSONAlgorithm
+
+
+def test_parameters():
+
+    alg = ConvertToTopoJSONAlgorithm()
+
+    alg.initAlgorithm()
+
+    assert alg.countVisibleParameters() == 3
+
+    parameter = alg.parameterDefinition("Input")
+
+    assert isinstance(parameter, QgsProcessingParameterVectorLayer)
+
+    parameter = alg.parameterDefinition("Fields")
+
+    assert isinstance(parameter, QgsProcessingParameterField)
+
+    parameter = alg.parameterDefinition("OutputFile")
+
+    assert isinstance(parameter, QgsProcessingParameterFileDestination)
+
+
+def test_outputs():
+
+    alg = ConvertToTopoJSONAlgorithm()
+
+    alg.initAlgorithm()
+
+    assert len(alg.outputDefinitions()) == 1
+
+    assert isinstance(alg.outputDefinitions()[0], QgsProcessingOutputFile)
+
+
+def test_input_data_temp_file(data_layer_path):
+
+    feedback = QgsProcessingFeedback()
+    context = QgsProcessingContext()
+
+    alg = ConvertToTopoJSONAlgorithm()
+
+    alg.initAlgorithm()
+
+    parameters = {"Input": data_layer_path, "Fields": [], "OutputFile": "TEMPORARY_OUTPUT"}
+
+    can_run, param_check_msg = alg.checkParameterValues(parameters=parameters, context=context)
+
+    assert param_check_msg == ""
+    assert can_run
+
+    result = alg.run(parameters=parameters, context=context, feedback=feedback)
+
+    assert isinstance(result, tuple)
+    assert result[1]
+    assert len(result[0]) == len(alg.outputDefinitions())
+    assert "OutputFile" in result[0].keys()
+
+    layer = QgsVectorLayer(result[0]["OutputFile"], "layer", "ogr")
+
+    assert isinstance(layer, QgsVectorLayer)
+    assert layer.featureCount() == 404
+
+
+def test_input_data_specified_file(data_layer_path, data_result_path):
+
+    feedback = QgsProcessingFeedback()
+    context = QgsProcessingContext()
+
+    alg = ConvertToTopoJSONAlgorithm()
+
+    alg.initAlgorithm()
+
+    data_result_file = Path(data_result_path) / "test.topojson"
+
+    parameters = {
+        "Input": data_layer_path,
+        "Fields": [],
+        "OutputFile": data_result_file.as_posix()
+    }
+
+    can_run, param_check_msg = alg.checkParameterValues(parameters=parameters, context=context)
+
+    assert param_check_msg == ""
+    assert can_run
+
+    result = alg.run(parameters=parameters,
+                     context=context,
+                     feedback=feedback,
+                     catchExceptions=False)
+
+    assert isinstance(result, tuple)
+    assert result[1]
+    assert len(result[0]) == len(alg.outputDefinitions())
+    assert "OutputFile" in result[0].keys()
+
+    layer = QgsVectorLayer(result[0]["OutputFile"], "layer", "ogr")
+
+    assert isinstance(layer, QgsVectorLayer)
+    assert layer.featureCount() == 404
