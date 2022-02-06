d0cdf7e9cd02e1fdc94f12ea04ba56d3e61fd437
Jan Caha <jan.caha@outlook.com>
2022-02-06 13:15:28 +0000

    add tests

---
+++ tests/classes/test_qmapshaper_command_builder.py
@@ -0,0 +1,58 @@
+from qmapshaper.classes.class_qmapshaper_command_builder import QMapshaperCommandBuilder
+
+
+def test_mapshaper_command_name():
+
+    assert QMapshaperCommandBuilder.mapshaper_command_name() == "mapshaper-xl"
+
+
+def test_mapshaper_command():
+
+    assert QMapshaperCommandBuilder.mapshaper_command() == "mapshaper-xl"
+
+
+def test_prepare_console_commands():
+
+    commands = QMapshaperCommandBuilder.prepare_console_commands("in.geojson", "out.geojson",
+                                                                 "simplify", ["dp", "20%"])
+
+    assert commands == [
+        'mapshaper-xl', 'in.geojson', '-simplify', 'dp', '20%', '-o', "format=geojson",
+        "out.geojson"
+    ]
+
+    commands = QMapshaperCommandBuilder.prepare_console_commands("in.shp", "out.shp", "simplify",
+                                                                 ["dp", "50%"])
+
+    assert commands == [
+        'mapshaper-xl', 'in.shp', '-simplify', 'dp', '50%', '-o', "format=shapefile", "out.shp"
+    ]
+
+    commands = QMapshaperCommandBuilder.prepare_console_commands("in.shp", "out.topojson",
+                                                                 "simplify", ["dp", "50%"])
+
+    assert commands == [
+        'mapshaper-xl', 'in.shp', '-simplify', 'dp', '50%', '-o', "format=topojson", "out.topojson"
+    ]
+
+
+def test_prepare_console_output_data():
+
+    assert QMapshaperCommandBuilder.prepare_console_output_data("out.topojson") == [
+        '-o', "format=topojson", "out.topojson"
+    ]
+
+    assert QMapshaperCommandBuilder.prepare_console_output_data("out.shp") == [
+        '-o', "format=shapefile", "out.shp"
+    ]
+
+    assert QMapshaperCommandBuilder.prepare_console_output_data("out.geojson") == [
+        '-o', "format=geojson", "out.geojson"
+    ]
+
+
+def test_prepare_console_tool_command():
+
+    assert QMapshaperCommandBuilder.prepare_console_tool_command("simplify") == "-simplify"
+
+    assert QMapshaperCommandBuilder.prepare_console_tool_command("clean") == "-clean"
---
+++ tests/classes/test_qmapshaper_data_preparer.py
@@ -0,0 +1,105 @@
+from pathlib import Path
+
+from qgis.core import (QgsVectorLayer)
+
+from qmapshaper.text_constants import TextConstants
+from qmapshaper.classes.class_qmapshaper_data_preparer import QMapshaperDataPreparer
+
+
+def test_copy_to_memory_layer(data_layer):
+
+    assert isinstance(data_layer, QgsVectorLayer)
+
+    copied_in_memory = QMapshaperDataPreparer.copy_to_memory_layer(data_layer)
+
+    assert isinstance(copied_in_memory, QgsVectorLayer)
+
+    assert copied_in_memory.crs().toWkt() == data_layer.crs().toWkt()
+    assert copied_in_memory.fields().names() == data_layer.fields().names()
+    assert copied_in_memory.featureCount() == data_layer.featureCount()
+    assert copied_in_memory.name() == data_layer.name()
+
+
+def test_write_layer_with_single_attribute(data_layer, data_result_path):
+
+    file = Path(data_result_path) / "output.shp"
+    file = file.as_posix()
+
+    QMapshaperDataPreparer.write_layer_with_single_attribute(data_layer, file, 1)
+
+    layer = QgsVectorLayer(file, "layer", "ogr")
+
+    assert layer
+
+    assert layer.crs().toWkt() == data_layer.crs().toWkt()
+    assert len(layer.fields().names()) == 1
+    assert layer.fields().names() == ["KOD_OBEC"]
+    assert layer.featureCount() == data_layer.featureCount()
+    assert layer.name() == data_layer.name()
+
+
+def test_write_layer_with_as_geojson(data_layer, data_result_path):
+
+    file = Path(data_result_path) / "output.geojson"
+    file = file.as_posix()
+
+    QMapshaperDataPreparer.write_layer_with_as_geojson(data_layer, file)
+
+    layer = QgsVectorLayer(file, "layer", "ogr")
+
+    assert layer
+
+    assert layer.crs().toWkt() == data_layer.crs().toWkt()
+    assert layer.fields().names() == data_layer.fields().names()
+    assert layer.featureCount() == data_layer.featureCount()
+    assert layer.name() == data_layer.name()
+
+
+def test_write_output_file(data_layer, data_result_path):
+
+    file = Path(data_result_path) / "output.gpkg"
+    file = file.as_posix()
+
+    QMapshaperDataPreparer.write_output_file(data_layer, file, layer_name="new name")
+
+    layer = QgsVectorLayer(file, "layer", "ogr")
+
+    assert layer
+
+    assert layer.crs().toWkt() == data_layer.crs().toWkt()
+    assert layer.fields().names() == data_layer.fields().names()
+    assert layer.featureCount() == data_layer.featureCount()
+
+
+def test_add_mapshaper_id_field(data_layer):
+
+    assert isinstance(data_layer, QgsVectorLayer)
+
+    copied_in_memory = QMapshaperDataPreparer.copy_to_memory_layer(data_layer)
+
+    QMapshaperDataPreparer.add_mapshaper_id_field(copied_in_memory)
+
+    assert TextConstants.JOIN_FIELD_NAME in copied_in_memory.fields().names()
+
+
+def test_join_fields_back(data_layer):
+
+    assert isinstance(data_layer, QgsVectorLayer)
+
+    copied_in_memory = QMapshaperDataPreparer.copy_to_memory_layer(data_layer)
+
+    field_index = QMapshaperDataPreparer.add_mapshaper_id_field(copied_in_memory)
+
+    fields = [x for x in range(copied_in_memory.fields().count())]
+
+    fields.remove(field_index)
+
+    copied_in_memory.startEditing()
+    copied_in_memory.deleteAttributes(fields)
+    copied_in_memory.commitChanges()
+
+    assert len(copied_in_memory.fields()) == 1
+
+    QMapshaperDataPreparer.join_fields_back(copied_in_memory, data_layer)
+
+    assert copied_in_memory.fields().count() == data_layer.fields().count() + 1
