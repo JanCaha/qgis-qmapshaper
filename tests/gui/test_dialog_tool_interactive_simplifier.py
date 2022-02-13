d4188d9cdd2e7aec565a12411a856aeffb99398f
Jan Caha <jan.caha@outlook.com>
2022-02-13 20:58:30 +0000

    divide test into two tests

--- tests/gui/test_dialog_tool_interactive_simplifier.py
+++ tests/gui/test_dialog_tool_interactive_simplifier.py
@@ -11,8 +11,34 @@ from qgis.PyQt.QtWidgets import (QSlider, QSpinBox, QDialogButtonBox, QComboBox)
 from qmapshaper.gui.dialog_tool_interactive_simplifier import InteractiveSimplifierTool
 
 
-def test(data_layer: QgsVectorLayer, qgis_iface: QgisInterface, qgis_parent,
-         qgis_canvas: QgsMapCanvas, qgis_new_project, qtbot: QtBot, qgis_app: QgsApplication):
+def test_elements(data_layer: QgsVectorLayer, qgis_canvas: QgsMapCanvas, qgis_iface: QgisInterface,
+                  qgis_parent):
+
+    qgs_project = QgsProject.instance()
+
+    qgs_project.addMapLayer(data_layer)
+
+    qgis_canvas.setProject(qgs_project)
+    qgis_canvas.zoomToFullExtent()
+
+    dialog = InteractiveSimplifierTool(qgis_parent, qgis_iface)
+
+    assert isinstance(dialog.percent_slider, QSlider)
+    assert isinstance(dialog.percent_spin_box, QSpinBox)
+    assert isinstance(dialog.layer_selection, QgsMapLayerComboBox)
+    assert isinstance(dialog.methods, QComboBox)
+    assert isinstance(dialog.canvas, QgsMapCanvas)
+    assert isinstance(dialog.button_box, QDialogButtonBox)
+
+    assert isinstance(dialog.map_updated, pyqtBoundSignal)
+    assert isinstance(dialog.data_generalized, pyqtBoundSignal)
+    assert isinstance(dialog.input_data_changed, pyqtBoundSignal)
+    assert isinstance(dialog.input_parameters_changed, pyqtBoundSignal)
+
+
+def test_usage(data_layer: QgsVectorLayer, qgis_iface: QgisInterface, qgis_parent,
+               qgis_canvas: QgsMapCanvas, qgis_new_project, qtbot: QtBot,
+               qgis_app: QgsApplication):
 
     assert isinstance(data_layer, QgsVectorLayer)
 
@@ -30,18 +56,6 @@ def test(data_layer: QgsVectorLayer, qgis_iface: QgisInterface, qgis_parent,
     qtbot.addWidget(dialog)
     dialog.show()
 
-    assert isinstance(dialog.percent_slider, QSlider)
-    assert isinstance(dialog.percent_spin_box, QSpinBox)
-    assert isinstance(dialog.layer_selection, QgsMapLayerComboBox)
-    assert isinstance(dialog.methods, QComboBox)
-    assert isinstance(dialog.canvas, QgsMapCanvas)
-    assert isinstance(dialog.button_box, QDialogButtonBox)
-
-    assert isinstance(dialog.map_updated, pyqtBoundSignal)
-    assert isinstance(dialog.data_generalized, pyqtBoundSignal)
-    assert isinstance(dialog.input_data_changed, pyqtBoundSignal)
-    assert isinstance(dialog.input_parameters_changed, pyqtBoundSignal)
-
     assert isinstance(dialog.layer_selection.currentLayer(), QgsVectorLayer)
 
     dialog.process.export_for_generalization()
@@ -51,8 +65,11 @@ def test(data_layer: QgsVectorLayer, qgis_iface: QgisInterface, qgis_parent,
 
     qtbot.waitUntil(test_1)
 
+    with qtbot.waitSignal(dialog.input_data_changed):
+        dialog.update_input_layer()
+
     with qtbot.waitSignal(dialog.map_updated):
-        dialog.generalize_layer()
+        dialog.percent_slider.setValue(10)
 
     assert dialog.canvas.layerCount() == 1
 
