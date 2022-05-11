57f708ece058a6d2bca5f8721f98cf869a43bf32
Jan Caha <jan.caha@outlook.com>
2022-05-11 13:26:21 +0200

    update process with selection

--- qmapshaper/gui/interactive_simplifier_process.py
+++ qmapshaper/gui/interactive_simplifier_process.py
@@ -18,6 +18,9 @@ from ..utils import log
 class InteractiveSimplifierProcess(QObject):
 
     memory_layer: QgsVectorLayer = None
+    stored_ids: List[int] = []
+    generalize_select: bool = False
+    generalize_select_features: bool = True
 
     input_data_filename: str = ""
 
@@ -43,7 +46,16 @@ class InteractiveSimplifierProcess(QObject):
 
     def set_input_data(self, layer: QgsVectorLayer) -> None:
 
+        if self.generalize_select:
+
+            self.stored_ids = layer.selectedFeatureIds()
+
+        generalize_field_index = QMapshaperDataPreparer.add_mapshaper_generalization_field(
+            layer, self.stored_ids, self.generalize_select_features)
+
         self.memory_layer = QMapshaperDataPreparer.copy_to_memory_layer(layer)
+
+        layer.removeExpressionField(generalize_field_index)
 
         self.input_layer_changed.emit()
 
@@ -51,11 +63,19 @@ class InteractiveSimplifierProcess(QObject):
 
         self.input_data_filename = QMapshaperFile.random_temp_filename()
 
-        field_index = QMapshaperDataPreparer.add_mapshaper_id_field(self.memory_layer)
+        field_index_id = QMapshaperDataPreparer.add_mapshaper_id_field(self.memory_layer)
 
-        QMapshaperDataPreparer.write_layer_with_single_attribute(layer=self.memory_layer,
-                                                                 file=self.input_data_filename,
-                                                                 col_index=field_index)
+        fields = [field_index_id]
+
+        if self.generalize_select and self.stored_ids:
+
+            field_index_generalization = self.memory_layer.fields().lookupField(
+                TextConstants.GENERALIZATION_FIELD_NAME)
+            fields.append(field_index_generalization)
+
+        QMapshaperDataPreparer.write_layer_with_minimal_attributes(layer=self.memory_layer,
+                                                                   file=self.input_data_filename,
+                                                                   col_index=fields)
         log(f"Data stored at: {self.input_data_filename}")
 
     def generalize_layer(self, simplify_percent: float, simplify_method: str) -> None:
@@ -69,9 +89,15 @@ class InteractiveSimplifierProcess(QObject):
 
         planar = not self.memory_layer.crs().isGeographic()
 
+        if self.generalize_select:
+            field_name = TextConstants.GENERALIZATION_FIELD_NAME
+        else:
+            field_name = None
+
         arguments = SimplifyAlgorithm.prepare_arguments(simplify_percent=simplify_percent,
                                                         method=simplify_method,
-                                                        planar=planar)
+                                                        planar=planar,
+                                                        field=field_name)
 
         commands = QMapshaperCommandBuilder.prepare_console_commands(
             input_data_path=self.input_data_filename,
@@ -99,11 +125,22 @@ class InteractiveSimplifierProcess(QObject):
         self._generalized_data_layer_memory = QMapshaperDataPreparer.copy_to_memory_layer(
             self._generalized_data_layer_memory)
 
+        fields_to_delete = []
+
         index = self._generalized_data_layer_memory.fields().lookupField(
             TextConstants.JOIN_FIELD_NAME)
 
+        fields_to_delete.append(index)
+
+        index = self._generalized_data_layer_memory.fields().lookupField(
+            TextConstants.GENERALIZATION_FIELD_NAME)
+
+        if -1 < index:
+
+            fields_to_delete.append(index)
+
         self._generalized_data_layer_memory.startEditing()
-        self._generalized_data_layer_memory.deleteAttribute(index)
+        self._generalized_data_layer_memory.deleteAttributes(fields_to_delete)
         self._generalized_data_layer_memory.commitChanges()
 
         self._generalized_data_layer_memory.setName("{} generalized".format(
@@ -153,3 +190,19 @@ class InteractiveSimplifierProcess(QObject):
         process.setArguments(commands)
 
         process.run()
+
+    def apply_selection_to_generalized_data(self) -> None:
+
+        index = self.generalized_data_only_geometry.fields().indexOf(
+            TextConstants.GENERALIZATION_FIELD_NAME)
+
+        value = 1
+
+        if not self.generalize_select_features:
+            value = 0
+
+        if -1 < index:
+
+            expr = "{} = {}".format(TextConstants.GENERALIZATION_FIELD_NAME, value)
+
+            self.generalized_data_only_geometry.selectByExpression(expr)
