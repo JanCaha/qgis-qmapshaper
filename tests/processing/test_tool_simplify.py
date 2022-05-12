4cd46d592b058ae88743f38fc66b05fbd140e867
Jan Caha <jan.caha@outlook.com>
2022-05-12 16:37:51 +0200

    better repeated test handling

--- tests/processing/test_tool_simplify.py
+++ tests/processing/test_tool_simplify.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+import pytest
 
 from qgis.core import (QgsProcessingParameterVectorLayer, QgsProcessingParameterNumber,
                        QgsProcessingParameterEnum, QgsProcessingParameterVectorDestination,
                        QgsProcessingFeedback, QgsProcessingContext, QgsProcessingOutputVectorLayer,
-                       QgsVectorLayer, QgsProcessingParameterField)
+                       QgsVectorLayer, QgsProcessingParameterField, QgsProcessingParameterBoolean)
 
 from qmapshaper.processing.tool_simplify import SimplifyAlgorithm
 
@@ -14,7 +15,7 @@ def test_parameters():
 
     alg.initAlgorithm()
 
-    assert alg.countVisibleParameters() == 5
+    assert alg.countVisibleParameters() == 6
 
     parameter = alg.parameterDefinition("Input")
 
@@ -35,6 +36,10 @@ def test_parameters():
     parameter = alg.parameterDefinition("Field")
 
     assert isinstance(parameter, QgsProcessingParameterField)
+
+    parameter = alg.parameterDefinition("CleanData")
+
+    assert isinstance(parameter, QgsProcessingParameterBoolean)
 
 
 def test_outputs():
@@ -57,75 +62,12 @@ def test_input_data_output_temp_file(data_layer):
 
     alg.initAlgorithm()
 
-    parameters = {"Input": data_layer, "Simplify": 12, "Method": 0, "Output": "TEMPORARY_OUTPUT"}
-
-    can_run, param_check_msg = alg.checkParameterValues(parameters=parameters, context=context)
-
-    assert param_check_msg == ""
-    assert can_run
-
-    result = alg.run(parameters=parameters, context=context, feedback=feedback)
-
-    assert isinstance(result, tuple)
-    assert result[1]
-    assert len(result[0]) == len(alg.outputDefinitions())
-    assert "Output" in result[0].keys()
-
-    layer = QgsVectorLayer(result[0]["Output"], "layer", "ogr")
-
-    assert isinstance(layer, QgsVectorLayer)
-    assert Path(layer.source()).exists()
-    assert layer.featureCount() == 404
-
-
-def test_input_data_output_named_file(data_layer, data_result_file):
-
-    feedback = QgsProcessingFeedback()
-    context = QgsProcessingContext()
-
-    alg = SimplifyAlgorithm()
-
-    alg.initAlgorithm()
-
-    parameters = {"Input": data_layer, "Simplify": 12, "Method": 0, "Output": data_result_file}
-
-    can_run, param_check_msg = alg.checkParameterValues(parameters=parameters, context=context)
-
-    assert param_check_msg == ""
-    assert can_run
-
-    result = alg.run(parameters=parameters,
-                     context=context,
-                     feedback=feedback,
-                     catchExceptions=False)
-
-    assert isinstance(result, tuple)
-    assert result[1]
-    assert len(result[0]) == len(alg.outputDefinitions())
-    assert "Output" in result[0].keys()
-
-    layer = QgsVectorLayer(result[0]["Output"], "layer", "ogr")
-
-    assert isinstance(layer, QgsVectorLayer)
-    assert Path(layer.source()).exists()
-    assert layer.featureCount() == 404
-
-
-def test_input_data_output_temp_with_field(data_layer, data_result_file):
-
-    feedback = QgsProcessingFeedback()
-    context = QgsProcessingContext()
-
-    alg = SimplifyAlgorithm()
-
-    alg.initAlgorithm()
-
     parameters = {
         "Input": data_layer,
         "Simplify": 12,
         "Method": 0,
-        "Field": "generalized",
-        "Output": data_result_file
+        "Output": "TEMPORARY_OUTPUT",
+        "CleanData": False
     }
 
     can_run, param_check_msg = alg.checkParameterValues(parameters=parameters, context=context)
@@ -134,6 +76,55 @@ def test_input_data_output_temp_with_field(data_layer, data_result_file):
     assert can_run
 
     result = alg.run(parameters=parameters, context=context, feedback=feedback)
+
+    assert isinstance(result, tuple)
+    assert result[1]
+    assert len(result[0]) == len(alg.outputDefinitions())
+    assert "Output" in result[0].keys()
+
+    layer = QgsVectorLayer(result[0]["Output"], "layer", "ogr")
+
+    assert isinstance(layer, QgsVectorLayer)
+    assert Path(layer.source()).exists()
+    assert layer.featureCount() == 404
+
+
+@pytest.mark.parametrize("params", [{
+    "Simplify": 12,
+    "Method": 0,
+    "CleanData": False
+}, {
+    "Simplify": 12,
+    "Method": 0,
+    "Field": "generalized",
+    "CleanData": False
+}, {
+    "Simplify": 12,
+    "Method": 0,
+    "Field": "generalized",
+    "CleanData": True
+}, {
+    "Simplify": 12,
+    "Method": 0,
+    "CleanData": True
+}])
+def test_parameter_combinations(params: dict, data_layer, data_result_file):
+
+    params.update({"Input": data_layer, "Output": data_result_file})
+
+    feedback = QgsProcessingFeedback()
+    context = QgsProcessingContext()
+
+    alg = SimplifyAlgorithm()
+
+    alg.initAlgorithm()
+
+    can_run, param_check_msg = alg.checkParameterValues(parameters=params, context=context)
+
+    assert param_check_msg == ""
+    assert can_run
+
+    result = alg.run(parameters=params, context=context, feedback=feedback)
 
     assert isinstance(result, tuple)
     assert result[1]
