20cf06aff10c3d38cbd8722c524ac3d4082fddd3
Jan Caha <jan.caha@outlook.com>
2022-05-12 16:22:03 +0200

    add options to clean before and after processing

--- qmapshaper/classes/class_qmapshaper_command_builder.py
+++ qmapshaper/classes/class_qmapshaper_command_builder.py
@@ -2,16 +2,25 @@ from typing import List
 
 from .class_qmapshaper_file import QMapshaperFile
 
+CLEAN_COMMAND = "-clean"
+
 
 class QMapshaperCommandBuilder:
 
     @staticmethod
-    def prepare_console_commands(input_data_path: str, output_data_path: str, command: str,
-                                 arguments: List[str]) -> List[str]:
+    def prepare_console_commands(input_data_path: str,
+                                 output_data_path: str,
+                                 command: str,
+                                 arguments: List[str],
+                                 clean_before: bool = False,
+                                 clean_after: bool = False) -> List[str]:
 
         commands = []
 
         commands.append(input_data_path)
+
+        if clean_before:
+            commands.append(CLEAN_COMMAND)
 
         if not command.startswith("-"):
             command = QMapshaperCommandBuilder.prepare_console_tool_command(command)
@@ -19,6 +28,9 @@ class QMapshaperCommandBuilder:
         commands.append(command)
 
         commands += arguments
+
+        if clean_after:
+            commands.append(CLEAN_COMMAND)
 
         commands += QMapshaperCommandBuilder.prepare_console_output_data(output_data_path)
 
